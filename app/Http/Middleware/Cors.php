ea8076f5b13af5ec6da01f4857c7e21e3badad19
kobayashi-m42 <m42kobayashi@gmail.com>
2018-10-21 14:45:28 +0900

    :sparkles: #40 Access-Control-Allow-HeadersにAuthorizationを追加

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,6 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', env('CORS_ORIGIN'))
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
