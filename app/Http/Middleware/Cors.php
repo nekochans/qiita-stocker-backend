13df2f297092d0e2f0e3a393d3bba9e4bbaa3d28
kobayashi-m42 <m42kobayashi@gmail.com>
2018-11-19 12:12:52 +0900

    :sparkles: #54 CORSの設定を修正

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', env('CORS_ORIGIN'))
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
