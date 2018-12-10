86c8d87c267e8fb48fb0ece22ecc374ae1cec5fa
kobayashi-m42 <m42kobayashi@gmail.com>
2018-12-10 17:34:24 +0900

    :sparkles: #57 アカウント作成APIにユーザ名のバリデーションを追加

--- app/Models/Domain/Account/AccountSpecification.php
+++ app/Models/Domain/Account/AccountSpecification.php
@@ -17,11 +17,12 @@ class AccountSpecification
      * @param array $requestArray
      * @return array
      */
-    public static function canCreate(array $requestArray): array
+    public static function canCreateQiitaAccountValue(array $requestArray): array
     {
         $validator = \Validator::make($requestArray, [
-            'accessToken' => 'required|regex:/^[a-z0-9]+$/|min:40|max:64',
-            'permanentId' => 'required|integer|min:1|max:4294967294',
+            'accessToken'    => 'required|regex:/^[a-z0-9]+$/|min:40|max:64',
+            'permanentId'    => 'required|integer|min:1|max:4294967294',
+            'qiitaAccountId' => 'required|min:1|max:191',
         ]);
 
         if ($validator->fails()) {
