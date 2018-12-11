815bc8857f57216a79cdc21bf631379c0b7306cb
kobayashi-m42 <m42kobayashi@gmail.com>
2018-12-11 22:07:56 +0900

    :sparkles: #5 ストックのマイグレーションファイルとEloquentを作成

---
+++ database/migrations/2018_12_11_170954_create_stocks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('account_id');
+            $table->string('article_id');
+            $table->text('title');
+            $table->string('user_Id');
+            $table->text('profile_image_url');
+            $table->dateTime('article_created_at');
+            $table->unsignedInteger('lock_version')->default(0);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->foreign('account_id', 'fk_stocks_01')->references('id')->on('accounts');
+            $table->index('account_id', 'idx_stocks_01');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
