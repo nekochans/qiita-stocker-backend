74c6ee12c116ee3bd1f0920f35489e53ab3e6469
kobayashi-m42 <m42kobayashi@gmail.com>
2018-12-20 00:49:04 +0900

    :sparkles: #59 セッションIDによる認証処理を作成

--- tests/Feature/LoginSessionDestroyTest.php
+++ tests/Feature/LoginSessionDestroyTest.php
@@ -39,6 +39,8 @@ class LoginSessionDestroyTest extends AbstractTestCase
     public function testSuccess()
     {
         $loginSession = '54518910-2bae-4028-b53d-0f128479e650';
+        $accountId = 1;
+        factory(LoginSession::class)->create(['id' => $loginSession, 'account_id' => $accountId, ]);
 
         $jsonResponse = $this->delete(
             '/api/login-sessions',
@@ -48,6 +50,72 @@ class LoginSessionDestroyTest extends AbstractTestCase
 
         // 実際にJSONResponseに期待したデータが含まれているか確認する
         $jsonResponse->assertStatus(204);
+        $jsonResponse->assertHeader('X-Request-Id');
+    }
+
+    /**
+     * 異常系のテスト
+     * Authorizationが存在しない場合エラーとなること
+     */
+    public function testErrorSessionNull()
+    {
+        $jsonResponse = $this->delete('/api/login-sessions');
+
+        // 実際にJSONResponseに期待したデータが含まれているか確認する
+        $expectedErrorCode = 401;
+        $jsonResponse->assertJson(['code' => $expectedErrorCode]);
+        $jsonResponse->assertJson(['message' => 'セッションが不正です。再度、ログインしてください。']);
+        $jsonResponse->assertStatus($expectedErrorCode);
+        $jsonResponse->assertHeader('X-Request-Id');
+    }
+
+    /**
+     * 異常系のテスト
+     * ログインセッションが不正の場合エラーとなること
+     */
+    public function testErrorSessionNotFound()
+    {
+        $loginSession = 'notFound-2bae-4028-b53d-0f128479e650';
+
+        $jsonResponse = $this->delete(
+            '/api/login-sessions',
+            [],
+            ['Authorization' => 'Bearer '.$loginSession]
+        );
+
+        // 実際にJSONResponseに期待したデータが含まれているか確認する
+        $expectedErrorCode = 401;
+        $jsonResponse->assertJson(['code' => $expectedErrorCode]);
+        $jsonResponse->assertJson(['message' => 'セッションが不正です。再度、ログインしてください。']);
+        $jsonResponse->assertStatus($expectedErrorCode);
+        $jsonResponse->assertHeader('X-Request-Id');
+    }
+
+    /**
+     * 異常系のテスト
+     * ログインセッションが有効期限切れの場合エラーとなること
+     */
+    public function testErrorSessionIsExpired()
+    {
+        $loginSession = '54518910-2bae-4028-b53d-0f128479e650';
+
+        factory(LoginSession::class)->create([
+            'id'         => $loginSession,
+            'account_id' => 1,
+            'expired_on' => '2018-10-01 00:00:00'
+        ]);
+
+        $jsonResponse = $this->delete(
+            '/api/login-sessions',
+            [],
+            ['Authorization' => 'Bearer '.$loginSession]
+        );
+
+        // 実際にJSONResponseに期待したデータが含まれているか確認する
+        $expectedErrorCode = 401;
+        $jsonResponse->assertJson(['code' => $expectedErrorCode]);
+        $jsonResponse->assertJson(['message' => 'セッションの期限が切れました。再度、ログインしてください。']);
+        $jsonResponse->assertStatus($expectedErrorCode);
         $jsonResponse->assertHeader('X-Request-Id');
     }
 }
