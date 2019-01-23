d8416ee2028fc95f54e41c020e9a01a47cd00f00
keitakn <keita.koga.work@gmail.com>
2019-01-24 02:36:55 +0900

    :recycle: #108 デプロイ実行時に実行する最適化コマンドを下記のドキュメントに載っている内容に修正
    https://readouble.com/laravel/5.7/ja/deployment.html

--- hooks/build.sh
+++ hooks/build.sh
@@ -4,15 +4,9 @@ source /home/ec2-user/.bash_profile
 
 cd /home/ec2-user/qiita-stocker-backend
 
-composer install
+composer install --optimize-autoloader --no-dev
 
 php artisan cache:clear
-
-composer dump-autoload --optimize
-
-php artisan clear-compiled
-
-php artisan optimize
 
 php artisan config:cache
 
