b372ad50219e38e19cb649f586f7e555c18d4959
keitakn <keita.koga.work@gmail.com>
2019-01-25 00:40:59 +0900

    :fire: #108 CORSエラーでAPIが動作しないのでCache系の動作を行わないように改修

--- hooks/build.sh
+++ hooks/build.sh
@@ -4,9 +4,4 @@ source /home/ec2-user/.bash_profile
 
 cd /home/ec2-user/qiita-stocker-backend
 
-composer install --optimize-autoloader --no-dev
-
-php artisan cache:clear
-
-php artisan config:cache
-
+composer install --optimize-autoloader
